5f21c0ee47c91ba3408dfc7d689495f6890a8801
Kashish <kathuriak805@gmail.com>
2025-07-19 00:43:32 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api", leaderboardRoutes);
 
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server running at port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
 })
